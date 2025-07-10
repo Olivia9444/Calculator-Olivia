9951624c7899361822db675a27033dec939e8c0c
Olivia Boone <144468291+Olivia9444@users.noreply.github.com>
2025-07-09 20:45:22 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Your JS code here
+var subTypeElement = document.querySelector("Subsciption");
+var subDurationElement = document.querySelector("months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function)
