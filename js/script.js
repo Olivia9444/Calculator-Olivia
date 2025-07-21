c6d7d76b4d53bce760da4f65a0690368a4fdc8e5
Olivia Boone <144468291+Olivia9444@users.noreply.github.com>
2025-07-21 20:20:31 +0000

    update with Java

--- js/script.js
+++ js/script.js
@@ -5,4 +5,26 @@ var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
 
-subTypeElement.addEventListener("change", function)
+subTypeElement.addEventListener("change", function (e)) {
+  subType = e.target.value;
+  // consolte.log(subType);
+  updateSubscriptionDiv();  
+});
+
+subDSurationElement.addEventListener("Change", function (e) {
+  subDuration = Number(e-target.value);
+  // console.loig(subDuration);
+  updateSubscriptionDiv();
+});
+
+var updateSubcriptionDiv = function ( ) {
+  var monthlyCost = 5; // for basic plan
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDueation * monthlyCost; 
+  result.innerTex = 'You have chosen a $ {subDuration} month ${subType} plan for $${total}.';
+
+};
