44bc46d31a91bca58b5702803acbe86f8527895b
Olivia Boone <144468291+Olivia9444@users.noreply.github.com>
2025-07-29 23:22:42 +0000

    new JS

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 // Your JS code here
-var subTypeElement = document.querySelector("Subsciption");
-var subDurationElement = document.querySelector("months");
+var subTypeElement = document.querySelector(".subsciption");
+var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
 
-subTypeElement.addEventListener("change"),
-  function (e) {
+subTypeElement.addEventListener("change",function (e) {
     subType = e.target.value;
-    // consolte.log(subType);
+    // console.log(subType);
     updateSubscriptionDiv();
-  };
+  });
 
-subDSurationElement.addEventListener("Change", function (e) {
-  subDuration = Number(e - target.value);
-  // console.loig(subDuration);
+subDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+   console.log(subDuration);
   updateSubscriptionDiv();
 });
 
-var updateSubcriptionDiv = function () {
+var updateSubscriptionDiv= function () {
   var monthlyCost = 5; // for basic plan
   if (subType === "standard") {
     monthlyCost = 7;
   } else if (subType === "premium") {
     monthlyCost = 10;
   }
-  var total = subDueation * monthlyCost;
-  result.innerTex =
-    "You have chosen a $ {subDuration} month ${subType} plan for $${total}.";
+  var total = subDuration * monthlyCost;
+  result.innerText =
+    'You have chosen a ${subDuration} month ${subType} plan for $${total}.';
 };
